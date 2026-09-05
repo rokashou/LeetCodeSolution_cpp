5c4afc27c538459143c3e685a1f4e0faf2a8ceaf
RoKashou <keima.sawamura@gmail.com>
2026-09-06 01:38:52 +0900

    122. Best Time to Buy and Sell Stock II
    Greedy

---
+++ Solution0122.cpp
@@ -0,0 +1,17 @@
+
+/* 122. Best Time to Buy and Sell Stock II */
+/* Runtime: 0ms, Memory: 20.08MB */
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) { 
+        int profit = 0; 
+        // Accumulate profit for every consecutive price increase
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+};
